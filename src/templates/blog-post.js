f8065c7940c49f980accf0eb4ff24260fa32ce32
Joey Dye <joey.m.dye@gmail.com>
2020-09-10 19:29:24 -0500

    feat: add image gallery

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import imageUrlBuilder from '@sanity/image-url'
 import BlockContent from '@sanity/block-content-to-react'
+import Gallery from 'react-grid-gallery'
 
 import Layout from '../components/layout'
 
 import { colors } from '../utils'
 
+const builder = imageUrlBuilder({
+  projectId: 'j7t5zwvc',
+  dataset: 'production',
+})
+
+function urlFor(source) {
+  return builder.image(source)
+}
+
 const serializers = {
   types: {
     block(props) {
-      console.log(props.children)
       switch (props.node.style) {
         default:
           return <p>{props.children}</p>
@@ -20,9 +30,17 @@ const serializers = {
 
 export default function BlogPost({
   data: {
-    sanityPost: { title, _rawBody },
+    sanityPost: { title, _rawBody, gallery },
   },
 }) {
+  const images = gallery[0]
+    ? gallery[0].images.map(image => ({
+        src: image.asset.url,
+        thumbnail: urlFor(image).width(400).height(400),
+        thumbnailWidth: 400,
+        thumbnailHeight: 400,
+      }))
+    : []
   return (
     <Layout>
       <div
@@ -58,6 +76,12 @@ export default function BlogPost({
             projectId="j7t5zwvc"
             dataset="production"
           />
+          <Gallery
+            images={images}
+            enableImageSelection={false}
+            margin={0}
+            rowHeight={200}
+          />
         </div>
       </div>
     </Layout>
@@ -70,6 +94,17 @@ export const pageQuery = graphql`
       id
       title
       _rawBody
+      gallery {
+        images {
+          asset {
+            url
+            fixed {
+              width
+              height
+            }
+          }
+        }
+      }
       body {
         _key
         _type
